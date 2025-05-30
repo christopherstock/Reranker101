1e3a6313d72dc0881bc59ba23429d0cc28f4375b
Christopher Stock <christopher.stock@mayflower.de>
2025-05-30 10:15:59 +0200

    python code on LlamaIndex

--- start.py
+++ start.py
@@ -1,19 +1,24 @@
-print("OK start.py invoked")
+# acclaim
+print("invoke start.py ", end='')
+print("OK")
 
+# disable warnings
+print("disable warnings ", end='')
 import warnings
 warnings.filterwarnings("ignore", message=r"^urllib3 v2 only supports OpenSSL")
-print("OK disabled warnings")
+print("OK")
 
-from llama_index.core import (
-    VectorStoreIndex,
-    SimpleDirectoryReader,
-)
-print("OK completed imports")
-
+# import libs
+print("import LlamaIndex lib ", end='')
 import os
-os.environ["OPENAI_API_KEY"] = "sk-"
-print("OK set OpenAPI key")
-
-print("OK Sys Lib Path:")
 import sys
-print(sys.path)
+from llama_index.core import ( VectorStoreIndex, SimpleDirectoryReader)
+print("OK")
+
+# assign OpenAPI key
+print("set OpenAPI key ", end='')
+os.environ["OPENAI_API_KEY"] = "sk-"
+print("OK")
+
+# print("OK Sys Lib Path:")
+# print(sys.path)
